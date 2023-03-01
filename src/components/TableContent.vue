99f419031fcf76232bde88a84a358b934c92008e
kimdayeon37 <kdy37912@naver.com>
2023-03-01 22:42:13 +0900

    first commit

---
+++ src/components/TableContent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from "vue";
+defineProps({ index: Number, randNum: Number });
+defineEmits(['divided-rand'])
+
+</script>
+
+<template>
+    <th>index</th>
+    <th>random</th>
+  <tr>
+    <td>{{ index }}</td>
+    <td>{{ randNum }}</td>
+    <button @click="$emit('divided-rand')">-1</button>
+  </tr>
+
+</template>
---
+++ src/views/MainPage.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TableContent from "../components/TableContent.vue";
+
+let index = 0;
+
+interface numberNumber {
+  index: number;
+  randNum: number;
+}
+const tableElements = ref<numberNumber[]>([]);
+const createRow = (e: MouseEvent) => {
+  console.log(e);
+  tableElements.value?.push({
+    index: index++,
+    randNum: Math.round(Math.random() * 10),
+  });
+};
+const deleteRow = () => {
+  console.log(index);
+  tableElements.value?.pop();
+};
+</script>
+
+<template>
+  <button @click="(e:MouseEvent)=>createRow(e)">+</button>
+  <button @click="deleteRow">-</button>
+  <hr/>
+  <table>
+    <TableContent
+      v-for="item in tableElements"
+      :key="item.index"
+      :index="item.index"
+      :randNum="item.randNum"
+      @divided-rand="item.randNum -= 1"
+    ></TableContent>
+  </table>
+</template>
